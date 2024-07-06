177f8a35eea1da8d625afc97ae1050d55d9a4db3
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-06 03:58:30 -0400

    SP5  @ModelAttribute

---
+++ src/main/java/com/sam/sp5_model_modelview_modelattribute/Sp5ModelModelViewModelAttributeApplication.java
@@ -0,0 +1,13 @@
+package com.sam.sp5_model_modelview_modelattribute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp5ModelModelViewModelAttributeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp5ModelModelViewModelAttributeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/sp5_model_modelview_modelattribute/Sp5ModelModelViewModelAttributeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.sp5_model_modelview_modelattribute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp5ModelModelViewModelAttributeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
